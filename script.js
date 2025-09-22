7f59010e63ab52890cef269cafb2ecb2786364f8
Saurabh Dombe <saurabhdombe83@gmail.com>
2025-09-22 11:45:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let squares = document.querySelectorAll(".square");
+
+squares.forEach((square, index) => {
+	square.addEventListener("mouseover", () => {
+		squares.forEach((sq, i) => {
+			if (i === index) {
+				sq.style.backgroundColor = "#E6E6FA";  // keep hovered 0ne levender
+			} else {
+				sq.style.backgroundColor = "#6F4E37";  // others coffee
+			}
+		});
+	});
+
+	square.addEventListner("mouseout", () => {
+		squares.forEach(sq => {
+			sq.style.backgroundColor = "#E6E6FA";
+		});
+	});
+});
+
